f4d1c6b037b41428d59c634e91e2ef9a7e1cec4e
RosydahRihadhatu <rosydahaisyiyah@gmail.com>
2024-07-19 14:17:20 +0700

    first commit

--- app/src/main/java/com/example/codeopus/api/GPTApiService.kt
+++ app/src/main/java/com/example/codeopus/api/GPTApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface GPTApiService {
-    @Headers("Content-Type: application/json", "Authorization: Bearer sk-proj-H0OjBhIUd7N3JjiAxzxLT3BlbkFJgKYVflpKncrLwPCmoKCM")
+    @Headers("Content-Type: application/json", "Authorization: Bearer sk......(secret key api gpt)")
     @POST("v1/chat/completions")
     fun translateCode(@Body requestBody: GPTChatRequest): Call<GPTChatResponse>
 }
