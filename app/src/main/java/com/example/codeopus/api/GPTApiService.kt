764234ff2ac9e5771da3beaf21336a77f2cee4ce
RosydahRihadhatu <rosydahaisyiyah@gmail.com>
2024-07-19 14:22:37 +0700

    commit lagi lagi

--- app/src/main/java/com/example/codeopus/api/GPTApiService.kt
+++ app/src/main/java/com/example/codeopus/api/GPTApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface GPTApiService {
-    @Headers("Content-Type: application/json", "Authorization: Bearer sk......(secret key api gpt)")
+    @Headers("rahasia eak")
     @POST("v1/chat/completions")
     fun translateCode(@Body requestBody: GPTChatRequest): Call<GPTChatResponse>
 }
